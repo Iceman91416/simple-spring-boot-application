d18e1912cf8edb3fd98720ee6675686d8d07899f
Iceman91416 <57415756+Iceman91416@users.noreply.github.com>
2022-05-07 12:13:30 +0200

    Update MainController.java

--- src/main/java/gr/codehub/simpleproject/controller/MainController.java
+++ src/main/java/gr/codehub/simpleproject/controller/MainController.java
@@ -8,6 +8,6 @@ public class MainController {
 
     @GetMapping("/")
     public String index(){
-        return "Wow! I am running perfectly! :O";
+        return "Wow! I am running perfectly! And I am changed";
     }
 }
